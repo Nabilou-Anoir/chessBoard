b78ef49d302b63a92d9b071f6689b12002c10102
Nabilou-Anoir <nabilou.anoir@etud.univ-jfc.fr>
2026-02-06 13:15:45 +0100

    correction test

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,17 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  test: {
+    environment: 'node',
+    exclude: [
+      'node_modules/**',
+      'dist/**',
+      '.git/**',
+      '.idea/**',
+      '.cache/**',
+      'output/**',
+      'temp/**',
+      'tests/**'
+    ]
+  }
 })
